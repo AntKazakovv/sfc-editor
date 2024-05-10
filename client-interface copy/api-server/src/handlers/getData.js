5f8c8f017e06129d81fd7407ae03163d9f0b7f66
anton.kazakov <anton.kazakov@thetoadman.com>
2024-05-10 14:20:10 +0200

    fix: delete copy folder

--- client-interface copy/api-server/src/handlers/getData.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const dataPath = './src/component_data.json';
-
-module.exports = (_req, res) => {
-  const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
-
-  res.json(data);
-}
--- client-interface copy/src/models/componentData.ts
+++
@@ -1,5 +0,0 @@
-export default interface ComponentData {
-  title: string,
-  imageUrl: string,
-  text: string
-}
